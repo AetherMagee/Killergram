35a119e373eb419203da4b5a6809a9e55c8eda99
AetherMagee <59086566+AetherMagee@users.noreply.github.com>
2023-09-22 17:24:13 +0300

    Remove Premium spoof as it was causing a lot if issues && Remove unnecessary app package check, xposed scope should already handle that for us

--- app/src/main/java/com/shatyuka/killergram/MainHook.java
+++ app/src/main/java/com/shatyuka/killergram/MainHook.java
@@ -1,7 +1,6 @@
 package com.shatyuka.killergram;
 
 import java.util.Arrays;
-import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -10,70 +9,47 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
-    public final static List<String> hookPackages = Arrays.asList(
-            "org.telegram.messenger",
-            "org.telegram.messenger.web",
-            "org.telegram.messenger.beta",
-            "tw.nekomimi.nekogram",
-            "nekox.messenger",
-            "com.cool2645.nekolite",
-            "org.telegram.plus",
-            "com.iMe.android",
-            "org.telegram.BifToGram",
-            "ua.itaysonlab.messenger",
-            "org.forkclient.messenger",
-            "org.forkclient.messenger.beta",
-            "org.aka.messenger",
-            "ellipi.messenger",
-            "org.nift4.catox",
-            "it.owlgram.android",
-            "com.exteragram.messenger");
-
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
-        if (hookPackages.contains(lpparam.packageName)) {
-            try {
-                // Working with MessageController - sponsored messages and forwarding
-                Class<?> messagesControllerClass = XposedHelpers.findClassIfExists("org.telegram.messenger.MessagesController", lpparam.classLoader);
-                if (messagesControllerClass != null) {
-                    XposedBridge.hookAllMethods(messagesControllerClass, "getSponsoredMessages", XC_MethodReplacement.returnConstant(null));
-                    XposedBridge.hookAllMethods(messagesControllerClass, "isChatNoForwards", XC_MethodReplacement.returnConstant(false));
-                } else {
-                    XposedBridge.log("[Killergram] messagesControllerClass not found");
-                }
-
-                // Working with ChatActivity - sponsored messages
-                Class<?> chatUIActivityClass = XposedHelpers.findClassIfExists("org.telegram.ui.ChatActivity", lpparam.classLoader);
-                if (chatUIActivityClass != null) {
-                    XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
-                } else {
-                    XposedBridge.log("[Killergram] chatUIActivityClass not found");
-                }
-
-                // Working with SharedConfig - performance class
-                Class<?> SharedConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.SharedConfig", lpparam.classLoader);
-                if (SharedConfigClass != null) {
-                    XposedBridge.hookAllMethods(SharedConfigClass, "getDevicePerformanceClass", XC_MethodReplacement.returnConstant(2));
-                } else {
-                    XposedBridge.log("[Killergram] SharedConfigClass not found");
-                }
-
-                // Working with UserConfig - acc count
-                Class<?> UserConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.UserConfig", lpparam.classLoader);
-                if (UserConfigClass != null) {
-                    XposedBridge.hookAllMethods(UserConfigClass, "getMaxAccountCount", XC_MethodReplacement.returnConstant(999));
-                    XposedBridge.hookAllMethods(UserConfigClass, "hasPremiumOnAccounts", XC_MethodReplacement.returnConstant(true));
-                    XposedBridge.hookAllMethods(UserConfigClass, "isPremium", XC_MethodReplacement.returnConstant(true));
-                } else {
-                    XposedBridge.log("[Killergram] UserConfigClass not found");
-                }
-
-
-                XposedBridge.log("[Killergram] Hook success for " + lpparam.packageName);
-            } catch (Throwable error) {
-                XposedBridge.log("[Killergram] Hook failure for " + lpparam.packageName);
-                XposedBridge.log(Arrays.toString(error.getStackTrace()));
+        try {
+            // Working with MessageController - sponsored messages and forwarding
+            Class<?> messagesControllerClass = XposedHelpers.findClassIfExists("org.telegram.messenger.MessagesController", lpparam.classLoader);
+            if (messagesControllerClass != null) {
+                XposedBridge.hookAllMethods(messagesControllerClass, "getSponsoredMessages", XC_MethodReplacement.returnConstant(null));
+                XposedBridge.hookAllMethods(messagesControllerClass, "isChatNoForwards", XC_MethodReplacement.returnConstant(false));
+            } else {
+                XposedBridge.log("[Killergram] messagesControllerClass not found");
             }
+
+            // Working with ChatActivity - sponsored messages
+            Class<?> chatUIActivityClass = XposedHelpers.findClassIfExists("org.telegram.ui.ChatActivity", lpparam.classLoader);
+            if (chatUIActivityClass != null) {
+                XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
+            } else {
+                XposedBridge.log("[Killergram] chatUIActivityClass not found");
+            }
+
+            // Working with UserConfig - acc count
+            Class<?> UserConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.UserConfig", lpparam.classLoader);
+            if (UserConfigClass != null) {
+                XposedBridge.hookAllMethods(UserConfigClass, "getMaxAccountCount", XC_MethodReplacement.returnConstant(999));
+                XposedBridge.hookAllMethods(UserConfigClass, "hasPremiumOnAccounts", XC_MethodReplacement.returnConstant(true));
+            } else {
+                XposedBridge.log("[Killergram] UserConfigClass not found");
+            }
+            // Working with SharedConfig - performance class
+//                Class<?> SharedConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.SharedConfig", lpparam.classLoader);
+//                if (SharedConfigClass != null) {
+//                    XposedBridge.hookAllMethods(SharedConfigClass, "getDevicePerformanceClass", XC_MethodReplacement.returnConstant(2));
+//                } else {
+//                    XposedBridge.log("[Killergram] SharedConfigClass not found");
+//                }
+
+
+            XposedBridge.log("[Killergram] Hooked successfully for " + lpparam.packageName);
+        } catch (Throwable error) {
+            XposedBridge.log("[Killergram] Unable to hook " + lpparam.packageName);
+            XposedBridge.log(Arrays.toString(error.getStackTrace()));
         }
     }
 }
